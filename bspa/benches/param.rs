a727ccdbea6ed950ba2390382c8443971460861c
Adam Barton <rosingrind@gmail.com>
2025-06-29 02:17:53 +0300

    bspa: rename benches

--- bspa/benches/param.rs
+++ bspa/benches/param.rs
@@ -13,7 +13,7 @@ const RECT_L: Rect = Rect::new(16, 16);
 const ITEMS: [Rect; 5] = [RECT_S, RECT_S, RECT_S, RECT_S, RECT_L];
 
 #[allow(non_snake_case)]
-fn cond<const BW: usize, const BB: usize>(b: &mut Bencher) {
+fn bw_bb<const BW: usize, const BB: usize>(b: &mut Bencher) {
     let node = BspaNode::new(ITEMS, 32, N, F);
 
     b.iter(|| {
@@ -24,10 +24,10 @@ fn cond<const BW: usize, const BB: usize>(b: &mut Bencher) {
 
 bencher::benchmark_group!(
     benches,
-    cond<10_000, 10_000>,
-    cond<10_000, 20_000>,
-    cond<10_000, 50_000>,
-    cond<20_000, 10_000>,
-    cond<50_000, 10_000>
+    bw_bb<10_000, 10_000>,
+    bw_bb<10_000, 35_000>,
+    bw_bb<10_000, 97_500>,
+    bw_bb<35_000, 10_000>,
+    bw_bb<97_500, 10_000>
 );
 bencher::benchmark_main!(benches);
